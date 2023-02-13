17eeedceb74b90c624d0ad29f9103e7db85dc465
Liloiscracked <mr.moodyaljoudy@gmail.com>
2023-02-13 17:29:32 +0300

    selection sort added

--- Sorter.java
+++ Sorter.java
@@ -1,6 +1,22 @@
 package LAB3;
 public class Sorter {
     public static void main(String[] args) {
-
+        int [] intarray = {5,2,22,1,0,-11,98,88};
+        selectionSort(intarray);
+        for(int elem : intarray)
+            System.out.println(elem);
+    }
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++){
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[index]){
+                    index = j;//searching for lowest index
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
     }
 }
