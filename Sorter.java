71e3435bb865196c9231eae1575bbd05dfb9eaaa
Liloiscracked <mr.moodyaljoudy@gmail.com>
2023-02-13 12:57:27 +0300

    version 1

---
+++ Sorter.java
@@ -0,0 +1,10 @@
+package LAB3;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Sorter {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
