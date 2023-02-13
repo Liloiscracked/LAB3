cc564bbcc76a0e85a1cdbfcdaf503309f35eeb57
Liloiscracked <mr.moodyaljoudy@gmail.com>
2023-02-13 17:08:31 +0300

    added Sorter file

--- Sorter.java
+++ Sorter.java
@@ -1,10 +1,6 @@
 package LAB3;
-
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Sorter {
     public static void main(String[] args) {
-        System.out.println("v1");
+
     }
 }
