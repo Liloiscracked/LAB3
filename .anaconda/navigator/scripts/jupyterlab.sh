909555c6da01f8141e0fd46ee6cbc72bd9de4ec3
Liloiscracked <mr.moodyaljoudy@gmail.com>
2023-11-29 16:28:06 +0300

    first

---
+++ .anaconda/navigator/scripts/jupyterlab.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /Users/medoaljoudi/opt/anaconda3/bin/activate /Users/medoaljoudi/opt/anaconda3
+jupyter lab >/Users/medoaljoudi/.anaconda/navigator/scripts/jupyterlab-out-1.txt 2>/Users/medoaljoudi/.anaconda/navigator/scripts/jupyterlab-err-1.txt
---
+++ .anaconda/navigator/scripts/pycharm_pro.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /Users/medoaljoudi/opt/anaconda3/bin/activate /Users/medoaljoudi/opt/anaconda3
+/Applications/PyCharm.app/Contents/MacOS/pycharm >/Users/medoaljoudi/.anaconda/navigator/scripts/pycharm_pro-out-1.txt 2>/Users/medoaljoudi/.anaconda/navigator/scripts/pycharm_pro-err-1.txt
